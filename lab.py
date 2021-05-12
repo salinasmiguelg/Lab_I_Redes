fc567f9c30241ba5e2a5415258696d444b4e6acb
maximiliano araya <maximiliano.araya@usach.cl>
2021-05-11 21:10:06 -0400

    puntos del 1 al 4 menos la anti

--- lab.py
+++ lab.py
@@ -5,8 +5,8 @@ from numpy import pi
 from scipy.fftpack import fft, fftfreq
 
 
-archivo = input('Ingrese archivo de sonido:' )
-frecuenciaM, datos = waves.read(archivo) # frecuencia del muestreo y los datos.
+#archivo = input('Ingrese archivo de sonido:' )
+frecuenciaM, datos = waves.read('G13.wav') # frecuencia del muestreo y los datos.
 
 
 
@@ -15,16 +15,10 @@ frecuenciaM, datos = waves.read(archivo) # frecuencia del muestreo y los datos.
 cantidad = np.shape(datos) # cantidad de datos en datos.
 muestras = cantidad[0]  # cantidad de datos del primer canal.
 m = len(cantidad) # si el largo es 2 entonces existen 2 canales
-canales = 1  #asumimos que es monofónico 
-if (m>1):  # estéreo
-	canales = cantidad[1]
-
-# experimento con un canal
-if (canales>1):
-    canal = 0
-    uncanal = datos[:,canal] 
-else:
-    uncanal = datos # se crea una copia de los datos
+#asumimos que es monofónico 
+if m==2:
+	print("El audio debe ser monofónico, un solo canal.")
+	exit()
 
 
 # rango de observación en segundos
@@ -33,38 +27,37 @@ termina = muestras/frecuenciaM # final en segundos del audio.
 
 
 
-# Salida # Archivo de audio.wav
-print('archivo de parte[] grabado...')
-waves.write('parte01.wav', frecuenciaM, datos)
-
-
-
-
-# Gráfica
-plt.plot(datos)
-plt.show()
-
-
 # tiempos en eje x
-dt = 1/frecuenciaM # periodo
-tab = np.arange(inicia,termina,dt)
-
-plt.plot(tab,datos)
-plt.xlabel('tiempo (s)')
-plt.ylabel('Amplitud')
+p = 1/frecuenciaM # periodo
+timeA = np.arange(inicia,termina,p) # arreglo para representar el tiempo.
+plt.plot(timeA,datos)
+plt.title('Audio sobre el tiempo')    
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Amplitud (db)')
+plt.savefig('fig1_audio_tiempo.png')
 plt.show()
 
 
 
 
-#n=2**15
+
 # frecuencia en eje x
-n=38912
-y=datos
-Y = fft(y) / n # Normalizada  TRans de f
-frq = fftfreq(n, dt) # Recuperamos las frecuencias
-plt.vlines(frq, 0, abs(Y)) # Espectro de amplitud
+n=muestras
+FFT = fft(datos) / muestras # Normalizada  TRans de f
+FFTfrq = fftfreq(muestras, p) # Recuperamos las frecuencias
+plt.vlines(FFTfrq, 0, abs(FFT)) # Espectro de amplitud
+plt.title('FFT, audio sobre la frecuencia') 
 plt.xlabel('Frecuencia (Hz)')
-plt.ylabel('Abs($Y$)')
+plt.ylabel('Amplitud FFT (db)')
+plt.savefig('fig2_audio_f.png')
 plt.show()
 
+
+# Espectograma.
+plt.title('Espectograma')    
+Pxx, freqs, bins, im = plt.specgram(datos,Fs=frecuenciaM,NFFT=152)
+plt.xlabel('Tiempo (s)')
+plt.ylabel('Frecuencia (Hz)')
+plt.xlim(left=0,right=5)
+plt.savefig('fig3_espectograma.png')
+plt.show()
